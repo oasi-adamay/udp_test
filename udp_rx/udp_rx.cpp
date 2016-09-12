a4b5ede498e8a4e7893f3f27f35f64e0367baee2
oasi-adamay <oasi.adamay@gmail.com>
2016-09-12 12:59:47 +0900

    init

---
+++ udp_rx/udp_rx.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <winsock2.h>
+
+
+#pragma comment(lib,"wsock32.lib")
+
+int main(void)
+{
+	WSAData wsaData;
+
+	SOCKET sock;
+	struct sockaddr_in addr;
+
+	char buf[2048];
+
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(12345);
+	addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+	memset(buf, 0, sizeof(buf));
+	recv(sock, buf, sizeof(buf), 0);
+
+	printf("%s\n", buf);
+
+	closesocket(sock);
+
+	WSACleanup();
+
+	return 0;
+}
+
+
---
+++ udp_tx/udp_tx.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib,"wsock32.lib")
+
+int main(void)
+{
+	WSAData wsaData;
+
+	SOCKET sock;
+	struct sockaddr_in addr;
+
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(12345);
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	sendto(sock, "HELLO", 5, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+	closesocket(sock);
+
+	WSACleanup();
+
+	return 0;
+}
+
